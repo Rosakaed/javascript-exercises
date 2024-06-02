360b39ea4e256f95437482776586f9fbf6b01163
Madhav Nath Khanna <83904569+Rosakaed@users.noreply.github.com>
2024-06-02 19:28:01 +0530

    Finish 02-06

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0)
+    {
+        return "ERROR";
+        }
+    
+    let ans = "";
+    for (let i = 0; i < times; i++)
+    {
+        ans += string;
+    }
+    return ans;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...arg) {
+    console.log("Hello")
+    for (let i = 0; i < arg.length; i++) {
+       
+        let j = 0;
+        while (j < array.length) {
+            if (arg[i] === array[j]) {
+                console.log(array[j]);
+                array.splice(j, 1);
+            }
+            else
+            {
+                j++;
+                }
+        }
+    }
+    return array;
 };
+
+
+console.log(removeFromArray([1, 2, 3, 4], 4))
 
 // Do not edit below this line
 module.exports = removeFromArray;
